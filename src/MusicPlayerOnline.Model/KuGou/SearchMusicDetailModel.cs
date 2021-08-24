1c223ea7999b6040ff21b913ca9cbf279fceb650
九零 <JiuLing-zhang@outlook.com>
2021-08-24 21:50:26 +0800

    优化
    1、删除不再使用的模型定义

--- src/MusicPlayerOnline.Model/KuGou/SearchMusicDetailModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MusicPlayerOnline.Model.KuGou
-{
-    public class SearchMusicDetailModel
-    {
-        public string Hash { get; set; }
-        public string AlbumId { get; set; }
-    }
-}
