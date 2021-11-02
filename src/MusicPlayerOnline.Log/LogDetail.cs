bdcc82347d7c558ea8d20852becdb14d5ccce0fb
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2021-11-02 22:43:42 +0800

    新增
    1、新增日志记录项目

---
+++ src/MusicPlayerOnline.Log/LogDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SQLite;
+
+namespace MusicPlayerOnline.Log
+{
+    public class LogDetail
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public Int64 Timestamp { get; set; }
+        public LogTypeEnum LogType { get; set; }
+        public string Message { get; set; }
+    }
+    public enum LogTypeEnum
+    {
+        消息 = 0,
+        警告 = 1,
+        错误 = 2
+    }
+}
---
+++ src/MusicPlayerOnline.Log/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using MusicPlayerOnline.Data;
+
+namespace MusicPlayerOnline.Log
+{
+    public class Logger
+    {
+        public static void Write(LogTypeEnum logType, string message)
+        {
+            try
+            {
+                var log = new LogDetail()
+                {
+                    Timestamp = JiuLing.CommonLibs.Text.TimestampUtils.GetLen13(),
+                    LogType = logType,
+                    Message = message
+                };
+                DatabaseProvide.Database.Insert(log);
+            }
+            catch (Exception ex)
+            {
+                //TODO 记录日志失败时，重试一波？
+            }
+        }
+
+        public static async Task WriteAsync(LogTypeEnum logType, string message)
+        {
+            try
+            {
+                var log = new LogDetail()
+                {
+                    Timestamp = JiuLing.CommonLibs.Text.TimestampUtils.GetLen13(),
+                    LogType = logType,
+                    Message = message
+                };
+                await DatabaseProvide.DatabaseAsync.InsertAsync(log);
+            }
+            catch (Exception ex)
+            {
+                //TODO 记录日志失败时，重试一波？
+            }
+        }
+    }
+}
